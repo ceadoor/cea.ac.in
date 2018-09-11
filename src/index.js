d76e128af6bdcdc428b2e3b6a514f39a289773a9
Ashik Meerankutty <ashik9591@gmail.com>
2018-09-11 23:31:42 +0530

    styled home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import 'bootstrap'
-import './sass/main.scss'
+import "bootstrap";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel";
+import "./sass/main.scss";
 
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel();
+});
