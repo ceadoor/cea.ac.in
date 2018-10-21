8962ee266cbffc2665343c073e8149d96992ac28
Ashik Meerankutty <ashik9591@gmail.com>
2018-10-21 12:29:23 +0530

    navbar bug fixes

--- src/index.js
+++ src/index.js
@@ -80,8 +80,10 @@ $(document).ready(function () {
   $(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
       $('nav').addClass('shrink');
+      $('#navbar-dropdown-hovered').addClass('shrink-dropdown')
     } else {
       $('nav').removeClass('shrink');
+      $('#navbar-dropdown-hovered').removeClass('shrink-dropdown')
     }
   });
 });
@@ -90,15 +92,15 @@ $( document ).ready( function() {
   $('.nav-holder').hover(function(){
   },
   function(){
-    $('#navbar-dropdown-hovered').html("")
-    $('nav').removeClass('nav-change');
+    // $('#navbar-dropdown-hovered').html("")
     $('#navbar-dropdown-hovered').animate({height: 0});
+    $('nav').removeClass('nav-change');
   }
   )
   $( '.dropdown' ).on( 'show.bs.dropdown mouseover', function() {
     $('nav').addClass('nav-change');
     $('#navbar-dropdown-hovered').html($( this ).find('.dropdown-menu').html())
-    $('#navbar-dropdown-hovered').animate({height: $('#navbar-dropdown-hovered').find('.dropdown-content').height() + 200});
+    $('#navbar-dropdown-hovered').animate({height: $('#navbar-dropdown-hovered').find('.dropdown-content').height() + 180});
   } );
   $('.dropdown').on( 'hide.bs.dropdown mouseleave', function(){
   } );
