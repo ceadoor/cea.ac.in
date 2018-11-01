3f9c290d766976ca33922d75e9fd376624453d30
Ashik Meerankutty <ashik9591@gmail.com>
2018-11-02 00:17:15 +0530

    Added accordion to course-plan added credits details styled course-page

--- src/index.js
+++ src/index.js
@@ -118,3 +118,18 @@ $window.scroll(function() {
       $('.course-navs').css('background',"transparent");
     }
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
