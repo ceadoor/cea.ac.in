23601d7d264af2201c9d6383aa9775edf8f42534
ashikmeerankutty <ashik9591@gmail.com>
2018-11-15 22:25:36 +0530

    changes to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,98 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
-const path = require("path");
-const globSync = require('glob').sync;
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const webpack = require("webpack")
+const path = require("path")
+const globSync = require("glob").sync
 
 module.exports = (env, options) => ({
-  entry: ["./src/index.js"],
-  devServer: {
-    contentBase: "./dist"
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          options.mode !== "production"
-            ? "style-loader"
-            : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|jpeg|svg)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "./img/"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: "html-loader",
-          options: {
-            attrs: [":src"]
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash].css",
-    }),
-    new CleanWebpackPlugin(["dist"]),
-    ...globSync('src/**/*.html').map((fileName) => {
-      return new HtmlWebpackPlugin({
-        template: fileName,
-        inject: "body",
-        filename: fileName.replace('src/', '')
-      })
-    }),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      Popper: ["popper.js", "default"],
-      Util: "exports-loader?Util!bootstrap/js/dist/util",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
-    })
-  ],
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: options.mode === "production" ?'https://ceadoor.github.io/cea.ac.in/':'http://localhost:8080/'
-  }
-});
+	entry: ["./src/index.js"],
+	devServer: {
+		contentBase: "./dist",
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					options.mode !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(png|jpg|gif|jpeg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "./img/",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(svg)$/,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "./img/",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(html)$/,
+				use: {
+					loader: "html-loader",
+					options: {
+						attrs: [":src"],
+					},
+				},
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["env"],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "css/[name].[contenthash].css",
+		}),
+		new CleanWebpackPlugin(["dist"]),
+		...globSync("src/**/*.html").map(fileName => {
+			return new HtmlWebpackPlugin({
+				template: fileName,
+				inject: "body",
+				filename: fileName.replace("src/", ""),
+			})
+		}),
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			"window.jQuery": "jquery",
+			Popper: ["popper.js", "default"],
+			Util: "exports-loader?Util!bootstrap/js/dist/util",
+			Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+		}),
+	],
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath:
+			options.mode === "production"
+				? "https://ceadoor.github.io/cea.ac.in/"
+				: "http://localhost:8080/",
+	},
+})
